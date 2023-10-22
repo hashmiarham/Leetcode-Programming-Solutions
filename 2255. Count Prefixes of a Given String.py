0ad18c318d85f852f0e6fe35f3e88f239322eac4
Arham Zafar Hashmi <75936363+hashmiarham123@users.noreply.github.com>
2023-10-22 20:38:52 +0530

    Add files via upload

---
+++ 2255. Count Prefixes of a Given String.py
@@ -0,0 +1,10 @@
+class Solution:
+    def countPrefixes(self, words: List[str], s: str) -> int:
+        count= 0 
+        for i in words:
+            if i ==s:
+                count+=1
+            else:
+                if i ==s[:len(i)]:
+                    count+=1
+        return count
