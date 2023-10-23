1065fb3f406a6e32683f7514a0adb9a41ceb217e
Arham Zafar Hashmi <75936363+hashmiarham123@users.noreply.github.com>
2023-10-23 15:26:12 +0530

    Add files via upload

---
+++ 1935. Maximum Number of Words You Can Type.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canBeTypedWords(self, text: str, brokenLetters: str) -> int:
+        count = 0
+
+        for word in text.split(" "):
+            check = True
+            for char in brokenLetters:
+                if char in word:
+                    check = False
+            
+            if check:
+                count += 1
+        
+        return count
+        
